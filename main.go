322123098300c92eaf8df5c41a5ebea4e5be5896
reiki4040 <3407000+reiki4040@users.noreply.github.com>
2024-05-10 00:36:28 +0900

    command usage

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ import (
 )
 
 var (
+	version  string
+	revision string
+
+	optUsage         bool
 	optSourceProfile string
 	optDuration      time.Duration
 )
@@ -24,10 +28,52 @@ var (
 func init() {
 	flag.StringVar(&optSourceProfile, "source-profle", "default", "switch source profile")
 	flag.DurationVar(&optDuration, "d", time.Hour, "duration seconds for session expire")
+	flag.BoolVar(&optUsage, "h", false, "show usage.")
+	flag.BoolVar(&optUsage, "help", false, "show usage.")
 	flag.Parse()
 }
 
+func showHelp() {
+	usage := `
+msk is assume role helper.
+you can set temporary assume role credentials to current zsh/bash.
+
+  eval $(msk <profile>)
+
+  * profile in ~/.aws/config
+
+[Usage]
+
+  assume role to profile and show credential export.
+
+  msk <profile>
+
+    export AWS_ACCESS_KEY_ID="<temporary credential>"
+    export AWS_SECRET_ACCESS_KEY="<temporary credential>"
+    export AWS_SESSION_TOKEN="<temporary credential>"
+    export AWS_SECURITY_TOKEN="<temporary credential>"
+    export ASSUMED_ROLE="<assumed role arn>"
+    export AWS_PROFILE="<target profile>"
+    # this temporary credentials expire at YYYY-MM-DDTHH:mm:ss
+
+[Optoins]
+`
+	usageLast := `
+see example:
+https://github.com/reiki4040/msk?tab=readme-ov-file#example
+`
+	fmt.Printf("msk %s[%s]\n", version, revision)
+	fmt.Println(usage)
+	flag.PrintDefaults()
+	fmt.Println(usageLast)
+}
+
 func main() {
+	if optUsage {
+		showHelp()
+		return
+	}
+
 	if len(flag.Args()) != 1 {
 		log.Fatal("required profile")
 	}
